167ed6b0c37e68e9b4c24351413b59419b7824e0
sy2729 <sy2729@tc.columbia.edu>
2018-03-11 20:18:35 -0400

    initialization

---
+++ server.js
@@ -0,0 +1,79 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var path = request.url
+  var query = ''
+  if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?')) }
+  var pathNoQuery = parsedUrl.pathname
+  var queryObject = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+
+
+
+
+if(path == '/index') {
+  response.setHeader('Content-Type', "text/html; charset=utf-8");
+  response.write(
+    "<!DOCTYPE html>" +
+    "<html>"+
+    "<head>"+
+    "<link rel='stylesheet' href='/style' >"+
+    "</heaed>"+
+    "<body>"+
+    "<h1>你好</h1>"+
+    '<script src="/script"></script>'+
+    "</body>"+
+    "</html>");
+  response.end();
+}else if(path == '/style') {
+  response.setHeader('Content-Type', "text/css");
+  response.write(
+    "h1 {color: red; text-align: center;}");
+  response.end();
+}else if(path == '/script') {
+  response.setHeader('Content-Type', "text/script");
+  response.write(
+    "alert('run js files')");
+  response.end();
+}else {
+  response.statusCode = 404;
+  response.end();
+};
+
+
+
+
+
+
+
+
+  console.log('方方说：得到 HTTP 路径\n' + path)
+  console.log('方方说：查询字符串为\n' + query)
+  console.log('方方说：不含查询字符串的路径为\n' + pathNoQuery)
+
+
+
+
+
+
+
+
+
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
